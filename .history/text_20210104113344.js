27273a149f5aa087524ed3501ee2e5a25ea23bd2
kim <beomsu9696@daum.net>
2021-01-04 12:02:05 +0900

    add text.js

---
+++ .history/text_20210104113344.js
@@ -0,0 +1,8 @@
+export class Text {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.canvas.style.position = "absolute";
+    this.canvas.style.left = "absolute";
+    this.canvas.style.top = "absolute";
+  }
+}
